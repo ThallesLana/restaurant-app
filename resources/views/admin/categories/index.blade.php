30795ff2d41f0f1652e0694aa432f731a1501eed
ThallesLana <thalles.lana1@gmail.com>
2022-06-27 15:34:48 -0300

    Controllers Admin

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,9 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+   <p>aaaaa</p>
+</x-admin-layout>
