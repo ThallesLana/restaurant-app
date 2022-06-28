3331c3d6688f727edfd57daa88c33aafac805e9a
ThallesLana <thalles.lana1@gmail.com>
2022-06-27 22:43:10 -0300

    Adding include category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-end m-2 p-2">
                 <a href="{{ route('admin.categories.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
-                    Create Category
+                    New Category
                 </a>
             </div>
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -14,8 +14,7 @@
         </div>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg bg-slate-200 space-y-8 divide-y w-1/2">
             <div class="m-2 p-2">
-                <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
-                    @csrf
+                <form enctype="multipart/form-data">
                     <!-- Name -->
                     <div class="relative z-0 w-full mb-6 group">
                         <input type="text" name="name" id="name"
@@ -26,7 +25,7 @@
                     </div>
                     <!-- Image -->
                     <div>
-                        <label for="image" class="block mb-2 text-sm font-medium text-gray-900">Upload Image</label>
+                        <label for="image" class="block mb-2 text-sm font-medium text-gray-900">Upload File</label>
                         <input type="file" id="image" name="image"
                             class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg p-1 cursor-pointer bg-slate-100 focus:outline-none">
                     </div>
@@ -36,12 +35,12 @@
                             class="block mb-2 text-sm font-medium text-gray-900">Description</label>
                         <textarea name="description" id="description" rows="3" placeholder="Leave a description..." class="block p-2.5 w-full text-sm text-gray-900 bg-slate-50 rounded-lg border-2 border-gray-300 focus:ring-indigo-500"></textarea>
                     </div>
-                    <div class="pt-5 pr-2 pb-0 pl-0.5">
-                        <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
-                            Store
-                        </button>
-                    </div>
                 </form>
+                <div class="pt-5 pr-2 pb-0 pl-0.5">
+                    <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
+                        Send
+                    </button>
+                </div>
             </div>
         </div>
     </div>
